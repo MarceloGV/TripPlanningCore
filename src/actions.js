c25f51f85b583e8af85264eaf7088603f5423897
Marcelo <marcelogvallejos@gmail.com>
2018-09-09 22:17:45 -0300

    Async api call added

--- src/actions.js
+++ src/actions.js
@@ -1,10 +1,38 @@
-export const GET_TRIPS = 'GET_TRIPS';
-export const CREATE_TRIP = 'CREATE_TRIP';
+import fetch from 'cross-fetch'
 
+export const GET_TRIPS = 'GET_TRIPS';
 export function getTrips() {
   return { type: GET_TRIPS }
 }
 
+export const CREATE_TRIP = 'CREATE_TRIP';
 export function createTrip(trip) {
   return { type: CREATE_TRIP, trip }
+}
+
+export const REQUEST_POSTS = 'REQUEST_POSTS'
+function requestPosts(subreddit) {
+  return {
+    type: REQUEST_POSTS
+  }
+}
+​
+export const RECEIVE_POSTS = 'RECEIVE_POSTS'
+function receivePosts(subreddit, json) {
+  return {
+    type: RECEIVE_POSTS,
+    posts: json.data.children.map(child => child.data),
+    receivedAt: Date.now()
+  }
+}
+
+export function fetchPosts(trips){
+  return function(dispatch){
+    dispatch(requestPosts(trips));
+    return fetch('localhost:9000')
+    .then(response => response.json, error => console.log('Error'))
+    .then(json => {
+      dispatch(receivePosts(trips, json));
+    });
+  }
 }
--- src/reducers.js
+++ src/reducers.js
@@ -30,8 +30,45 @@ function trips(state = [], action) {
   }
 }
 
+function posts( state = {
+    isFetching: false,
+    didInvalidate: false,
+    items: []
+  },
+  action){
+    switch (action.type) {
+      case REQUEST_POSTS:
+        return Object.assign({}, state, {
+          isFetching: true,
+          didInvalidate: false
+        })
+      case RECEIVE_POSTS:
+        return Object.assign({}, state, {
+          isFetching: false,
+          didInvalidate: false,
+          items: action.posts,
+          lastUpdated: action.receivedAt
+        })
+      default:
+        return state
+    }
+  }
+
+  function postsTrips(state = {}, action) {
+    switch (action.type) {
+      case RECEIVE_POSTS:
+      case REQUEST_POSTS:
+        return Object.assign({}, state, {
+          [action.subreddit]: posts(state[action.subreddit], action)
+        })
+      default:
+        return state
+    }
+  }
+
 const tripsApp = combineReducers({
-  trips
+  trips,
+  postsTrips
 });
 
 export default tripsApp;
